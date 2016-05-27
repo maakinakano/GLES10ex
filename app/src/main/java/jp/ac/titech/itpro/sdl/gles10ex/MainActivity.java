594be7fae105ec556b40d4d7c53c5172da84d46d
maaki <hitotubashiyurie>
2016-05-27 17:12:54 +0900

    rotate gravity(one direction)

--- app/src/main/java/jp/ac/titech/itpro/sdl/gles10ex/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/gles10ex/MainActivity.java
@@ -1,18 +1,26 @@
 package jp.ac.titech.itpro.sdl.gles10ex;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.SeekBar;
 
 
-public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
     private final static String TAG = "MainActivity";
 
     private GLSurfaceView glView;
     private SimpleRenderer renderer;
-    private SeekBar rotationBarX, rotationBarY, rotationBarZ;
+    private SensorManager sensorManager;
+    private Sensor gravityMeter;
+    private float X,dX;
+    private RotateThread th = null;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,18 +29,21 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         setContentView(R.layout.activity_main);
         glView = (GLSurfaceView) findViewById(R.id.glview);
 
-        rotationBarX = (SeekBar) findViewById(R.id.rotation_bar_x);
-        rotationBarY = (SeekBar) findViewById(R.id.rotation_bar_y);
-        rotationBarZ = (SeekBar) findViewById(R.id.rotation_bar_z);
-        rotationBarX.setOnSeekBarChangeListener(this);
-        rotationBarY.setOnSeekBarChangeListener(this);
-        rotationBarZ.setOnSeekBarChangeListener(this);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        gravityMeter = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+        if (gravityMeter == null) {
+            finish();
+            return;
+        }
 
         renderer = new SimpleRenderer();
         renderer.addObj(new Cube(0.5f, 0, 0.2f, -3));
         renderer.addObj(new Pyramid(0.5f, 0, 0, 0));
         renderer.addObj(new Pentagonal(0.5f, 0.5f, 0.5f, 0.5f));
         glView.setRenderer(renderer);
+        X =  0F;
+        dX = 0F;
+        handler = new Handler();
     }
 
     @Override
@@ -40,6 +51,9 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         super.onResume();
         Log.d(TAG, "onResume");
         glView.onResume();
+        th = new RotateThread();
+        th.start();
+        sensorManager.registerListener(this, gravityMeter, SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     @Override
@@ -47,24 +61,36 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         super.onPause();
         Log.d(TAG, "onPause");
         glView.onPause();
+        th = null;
+        sensorManager.unregisterListener(this);
     }
 
     @Override
-    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-        if (seekBar == rotationBarX)
-            renderer.setRotationX(progress);
-        else if (seekBar == rotationBarY)
-            renderer.setRotationY(progress);
-        else if (seekBar == rotationBarZ)
-            renderer.setRotationZ(progress);
+    public void onSensorChanged(SensorEvent event) {
+        dX = event.values[1];
     }
 
     @Override
-    public void onStartTrackingTouch(SeekBar seekBar) {
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
 
-    @Override
-    public void onStopTrackingTouch(SeekBar seekBar) {
+    private class RotateThread extends Thread {
+        public void run() {
+            try {
+                while (th != null) {
+                    handler.post(new Runnable() {
+                        public void run() {
+                            X += dX;
+                            renderer.setRotationX(X);
+                        }
+                    });
+                    Thread.sleep(20);
+                }
+            }
+            catch (InterruptedException e) {
+                Log.e(TAG, e.toString());
+                th = null;
+            }
+        }
     }
-
 }
